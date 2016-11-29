6c7f65f327110cc631be6a42c51f02c04b69aeb0
Eujine <sponsor85@mail.ru>
2016-11-29 03:33:46 +0200

    fix [] method
    fix [] method  use global args

--- factory.rb
+++ factory.rb
@@ -3,14 +3,14 @@ class Factory
     Class.new do      
       attr_accessor( *args)  
       class_eval(&block) if block_given?
-      define_method :initialize do |*values|
-        @values = values        
-        args.zip(values).each do |name, value|  
+      define_method(:initialize) do |*values|
+        args.zip(values).each do |name, value|
           send("#{name}=",value)
         end
       end
-      def [](index)
-        (index.is_a?Fixnum) ? @values[index] : send(index)
+      define_method(:[]) do |index|
+        (index.is_a? Numeric) ? field = args[index] : field = index
+        send(field)
       end
     end
   end
